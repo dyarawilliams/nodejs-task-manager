7ac86b5642db99e696eaf1e92e539f0e94c68b71
D'yara Williams <dyara.williams@gmail.com>
2022-03-16 18:27:53 -0400

    set up port in app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+// Routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+
+// Get all tasks -  app.get()
+// Create a new task -- app.post() 
+// Get single task -- app.get()
+// Update task -- app.patch()
+// Delete Task -- app.delete()
+
+const port = 3000
+
+app.listen(port, console.log(`Server is listening on port ${port}`))
