e8d9bac87c467d73b9ad0b0f0a3c75aa25f7bd07
D'yara Williams <dyara.williams@gmail.com>
2022-03-17 00:26:32 -0400

    add middleware and starter route

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express()
+const tasks = require('./routes/tasks')
+
+// Middleware
+app.use(express.static('./public'));
+app.use(express.json())
+
 
 // Routes
 app.get('/hello', (req, res) => {
     res.send('Task Manager App')
 })
 
-// Get all tasks -  app.get()
-// Create a new task -- app.post() 
-// Get single task -- app.get()
-// Update task -- app.patch()
-// Delete Task -- app.delete()
+app.use('/api/v1/tasks', tasks)
+
+// Get all tasks -  app.get('/api/v1/tasks')
+// Create a new task -- app.post('/api/v1/tasks') 
+// Get single task -- app.get('/api/v1/tasks'/:id)
+// Update task -- app.patch('/api/v1/tasks/:id')
+// Delete Task -- app.delete('/api/v1/tasks/:id')
 
 const port = 3000
 
